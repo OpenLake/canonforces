7f0d8d41e2c699ff59a893fa699515ec719bd8cc
Kanish Bodhwani <kanishstark786@gmail.com>
2022-12-18 19:45:32 +0530

    Created UI of homepage

---
+++ canonforces/src/constants/routes.tsx
@@ -0,0 +1,6 @@
+export const HOME = "/";
+export const LOGIN = "/login";
+export const DASHBOARD = "/dashboard";
+export const USERS = "/users";
+export const NOT_FOUND = "/not-found";
+export const PROFILE = "/user/:id";
