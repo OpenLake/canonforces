ba5dbc562743fa3face94820236e10f7e7c2bc00
Kanish Bodhwani <kanishstark786@gmail.com>
2023-01-17 16:12:48 +0530

    Authentication complete

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,9 +1,22 @@
-import { signInWithPopup } from "firebase/auth"
-import { auth , provider} from "../lib/firebase"
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../lib/firebase";
 
+export const signupWithGoogle = async () => {
+    signInWithPopup(auth, provider)
+    .then(res => {
+        if(res) {
+            return res.user;
+        } else {
+            return null;
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    })
+}
 
 export const doesUsernameExists = async (username: string)=> {
-    const user = await fetch(`https://codeforces.com/api/user.info?handle=${username}`);
+    const user = await fetch(`https://codeforces.com/api/user.info?handles=${username}`);
     console.log(user);
     if(user.status === 200) {
         return user;
