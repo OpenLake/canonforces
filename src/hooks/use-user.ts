f2f65970c84185e1ec1fb407e6007380be1196d2
Kanish Bodhwani <kanishstark786@gmail.com>
2023-01-19 10:06:16 +0530

    Issue 16: User Profile

---
+++ src/hooks/use-user.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect, useContext } from "react";
+import UserContext from "../context/user";
+import { getUserByUserId } from "../services/firebase";
+
+export default function useUser() {
+  const [activeUser, setAvtiveUser] = useState();
+  const user = useContext(UserContext);
+
+  useEffect(() => {
+    async function getUserObjByUserId() {
+      // we need a func that gets user based on the id
+      const [response] = await getUserByUserId(user.uid);
+      setAvtiveUser(response);
+    }
+    if (user?.uid) {
+      getUserObjByUserId();
+    }
+    ;
+  }, [user]);
+
+  return { user: activeUser };
+}
--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,6 +1,6 @@
 import NavigationMenu from "../../common/components/NavigationMenu/NavigationMenu";
 import Profile from "../../common/components/Profile/Profile";
-import styles from "../styles/Dashboard.module.css";
+
 
 export default function user() {
   return (
