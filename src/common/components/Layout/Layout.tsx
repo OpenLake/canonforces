014ddd70dad718d70a1b1f13400f7707fd117c54
Aviral Saxena <aviralsaxena2006@gmail.com>
2025-06-16 01:38:46 +0530

    fixing conflict

--- src/common/components/Layout/Layout.tsx
+++ src/common/components/Layout/Layout.tsx
@@ -3,11 +3,20 @@ import styles from './Layout.module.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
+<<<<<<< HEAD
    
     <div className={styles.layout}>
       <NavigationMenu />
     <main className={styles.main}>{children}</main>
     </div>
      
+=======
+    <>
+    <div className={styles.layout}>
+      <NavigationMenu />
+    </div>
+      <main className={styles.main}>{children}</main>
+      </>
+>>>>>>> 83e4f7c (Sidebar is now fixed)
   );
 }
