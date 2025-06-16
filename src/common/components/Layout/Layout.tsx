f27a5a91a9ebe3d3a73ab9cf5ce0eb36d3f7e674
Aviral Saxena <aviralsaxena2006@gmail.com>
2025-06-16 15:02:04 +0530

    Sidebar issue fixed

--- src/common/components/Layout/Layout.tsx
+++ src/common/components/Layout/Layout.tsx
@@ -3,11 +3,11 @@ import styles from './Layout.module.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+   
     <div className={styles.layout}>
       <NavigationMenu />
+    <main className={styles.main}>{children}</main>
     </div>
-      <main className={styles.main}>{children}</main>
-      </>
+     
   );
 }
