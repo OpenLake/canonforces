3928afe401e755c058bee67294f46cb9dfc1aae0
Aviral Saxena <aviralsaxena2006@gmail.com>
2025-08-02 22:49:43 +0530

    Production ready with normal login

--- src/pages/signup/[username].tsx
+++ src/pages/signup/[username].tsx
@@ -1,57 +1,55 @@
 import { useState, useContext } from "react";
 import styles from "../../styles/Signup.module.css";
-import { BsPatchExclamation, BsPatchCheck} from "react-icons/bs";
+import { BsPatchExclamation, BsPatchCheck } from "react-icons/bs";
 import { useRouter } from "next/router";
 import { User } from "../../types/user";
 import UserContext from "../../context/user";
 import { db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
 
-
-export default function Username({}){
+export default function Username({}) {
     const [username, setUsername] = useState('');
     const authUser = useContext(UserContext);
 
-    const [error, setError] = useState(''); 
+    const [error, setError] = useState('');
     const isInvalid = username === '';
 
     const router = useRouter();
 
-    const getData = async () => { 
-        // const currentTime = new Date().getTime();
-        // console.log(currentTime);
-    
-        // const randomNumber = (): number => Math.floor(Math.random() * 1000000);
-        // sha512Hex(${randomNumber}/user.info?apiKey=5bb4c6ecab438b6aa0beff73ac3e257115afb642&time=${currentTime}#874a1d7cde02549386ef85897bb65bf7d3ca4060)
-        try {
-            const res = await fetch(`https://codeforces.com/api/user.info?handles=${username}`);
-            const data = await res.json(); 
-            console.log(data.result[0]);
-            if(data) {
-                console.log(authUser);
-
-                const newUser: User = {
-                    fullname: authUser.displayName,
-                    userId: authUser.uid,
-                    username: data.result[0].handle,
-                    dateCreated: new Date(),
-                    email: authUser.email,
-                    avatar: data.result[0].avatar,
-                    followers: [],
-                    following: []
-                }
-
-                setDoc(doc(db, "users"), newUser) 
-                    .then((res) => {
-                        console.log(res, "Written succesfully");
-                    });
-            }
-
-        } catch (err: any) {
-            setError(err.message);
-        }        
-        
-    }
+    // const getData = async () => { 
+    //     // const currentTime = new Date().getTime();
+    //     // console.log(currentTime);
+    // 
+    //     // const randomNumber = (): number => Math.floor(Math.random() * 1000000);
+    //     // sha512Hex(${randomNumber}/user.info?apiKey=5bb4c6ecab438b6aa0beff73ac3e257115afb642&time=${currentTime}#874a1d7cde02549386ef85897bb65bf7d3ca4060)
+    //     try {
+    //         const res = await fetch(`https://codeforces.com/api/user.info?handles=${username}`);
+    //         const data = await res.json(); 
+    //         console.log(data.result[0]);
+    //         if(data) {
+    //             console.log(authUser);
+    // 
+    //             const newUser: User = {
+    //                 fullname: authUser.displayName,
+    //                 userId: authUser.uid,
+    //                 username: data.result[0].handle,
+    //                 dateCreated: new Date(),
+    //                 email: authUser.email,
+    //                 avatar: data.result[0].avatar,
+    //                 followers: [],
+    //                 following: []
+    //             }
+    // 
+    //             setDoc(doc(db, "users"), newUser) 
+    //                 .then((res) => {
+    //                     console.log(res, "Written succesfully");
+    //                 });
+    //         }
+    // 
+    //     } catch (err: any) {
+    //         setError(err.message);
+    //     }        
+    // }
 
     return (
         <div className={styles.signup}>
@@ -69,12 +67,12 @@ export default function Username({}){
                         {username ? <BsPatchCheck className={styles.check__icon}/> : <BsPatchExclamation className={styles.exclaimation__icon}/>}
                     </div>
                     <div className={styles.form__buttons}>
-                        <button className={styles.login__button} onClick={() => getData()} > Enter </button>
+                        {/* <button className={styles.login__button} onClick={() => getData()} > Enter </button> */}
+                        <button className={styles.login__button} disabled> Enter </button>
                     </div>
                 </div>
             </div>  
-            {/* <pre>{user.res} </pre> */}
+             {/* <pre>{user.res} </pre> */}
         </div>
     )
 };
-
