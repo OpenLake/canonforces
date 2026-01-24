e51da2a24ae4fe820413a017cffbbb05dd862f22
Pratyush Kumar <prtsh2005@gmail.com>
2026-01-25 01:25:37 +0530

    refactor: improve landing page UI based on review feedback - Remove NotificationBell from landing page header (unauthenticated users) - Improve mascot image quality and sharpness - Maintain exact character design and pose (mirrored orientation) Addresses reviewer feedback from @aviralsaxena16

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,28 +1,23 @@
 import Head from "next/head";
 import Link from "next/link";
 import * as ROUTES from "../../../constants/routes";
-import NotificationBell from "../NotificationBell/NotificationBell";
 import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <>
       <div className={`flex w-10/12 p-5 fc-black font-light max-w-screen-2xl`}>
-        <nav className="flex flex-row w-full">
-          <div className="basis-1/4 flex items-center justify-center">
-            <ul className="flex-row flex w-full justify-around">
-              <li> <Link href={ROUTES.DASHBOARD}> Dashboard </Link> </li>
-              <li> <Link href={ROUTES.USERS}> Users </Link> </li>
-            </ul>
+        <nav className="flex flex-row w-full items-center justify-between">
+          {/* Left side - Logo */}
+          <div className="flex items-center">
+            <Link href={ROUTES.HOME} className="font-semibold text-xl">
+              CANONFORCES
+            </Link>
           </div>
-          <div className="basis-1/2 flex items-center justify-center">
-            <ul className="flex-row flex w-full justify-around">
-              <li className="font-semibold text-xl"> <Link href={ROUTES.HOME}> CANONFORCES </Link> </li>
-            </ul>
-          </div>
-          <div className="basis-1/4 flex items-center justify-center">
-            <ul className="flex-row flex w-full justify-around items-center">
-              <li> <NotificationBell /> </li>
+
+          {/* Right side - Signup, Login */}
+          <div className="flex items-center">
+            <ul className="flex-row flex gap-6 items-center">
               <li> <Link href={ROUTES.SIGNUP}> Signup </Link> </li>
               <li> <Link href={ROUTES.LOGIN}> Login </Link> </li>
             </ul>
