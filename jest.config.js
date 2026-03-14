9837abecf50c5ff671dcb210cfd72eea5442e8aa
Aviral Saxena <aviralsaxena2006@gmail.com>
2026-03-14 16:58:38 +0530

    Updating the pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-/** @type {import('jest').Config} */
-const config = {
+module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
   collectCoverage: true,
   coverageDirectory: "coverage",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  clearMocks: true,
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"]
 };
-
-module.exports = config;
